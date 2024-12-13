ff6c9015c9154c7bad65e88a233d4d9e2fb35103
zi1dd <zff3788@gmail.com>
2024-12-13 02:03:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1244273472792957022
+CHANNEL_ID = 1316292695974608946
 SELF_MUTE = True
 SELF_DEAF = False
 
